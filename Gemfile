27888fe2c5ba1c1ab8b581c7ac1cadc2b53438d9
srp2930 <s.rimingtonpounder@gmail.com>
2016-06-14 11:35:24 +0100

    First feature test

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+# A sample Gemfile
+source "https://rubygems.org"
+
+ruby '2.3.0'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+end
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'rspec-sinatra'
+
+
+
+
+# gem "rails"
---
+++ spec/features/view_links_spec.rb
@@ -0,0 +1,14 @@
+# As a time-pressed user
+# So that I can quickly go to web sites I regularly visit
+# I would like to see a list of links on the homepage
+
+feature 'view links' do
+  scenario 'view list of links on homepage' do
+    Links.create(url: 'http://www.salamanca24horas.com', title: 'Noticias Salamanca')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Salamanca')
+    end
+  end
+end
