a51d064465535275d6b3fd9f87e8df91dd718f18
Stephen Dawes <sdawes@outlook.com>
2016-06-15 16:09:27 +0100

    add ability to filter by tags

--- app.rb
+++ app.rb
@@ -28,6 +28,12 @@ class BookmarkManager < Sinatra::Base
     redirect '/links'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'index'
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/see_list_of_links_spec.rb
+++ spec/features/see_list_of_links_spec.rb
@@ -9,4 +9,18 @@ feature 'present a list of links on page' do
       expect(page).to have_content('Makers Academy')
     end
   end
+
+  before(:each) do
+    Link.create(title: 'Makers Academy', url: 'http://www.makersacademy.com', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(title: 'Google', url: 'http://www.google.com', tags: [Tag.first_or_create(name: 'search_engine')])
+  end
+
+  scenario 'filtering links by tag' do
+    visit '/tags/search_engine'
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+      expect(page).not_to have_content('Makers Academy')
+    end
+  end
 end
