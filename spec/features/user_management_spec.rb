fb5a4a56c288d166f2ab0d4a987b0e3a1e87750e
timchipperfield <timchipperfield@gmail.com>
2016-06-16 15:59:35 +0100

    add password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,4 +6,16 @@ feature 'user sign up' do
     expect(page).to have_content('Welcome, poppy@email.com')
     expect(User.first.email).to eq('poppy@email.com')
   end
+
+  scenario "requires a matching password confirmation" do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'poppy@email.com', password: '1234', password_confirmation: '1234')
+    visit('/users/new')
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
 end
